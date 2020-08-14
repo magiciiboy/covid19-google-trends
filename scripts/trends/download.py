e3356cac3ab648921ff92909733909744bd59c34
Tung Huynh <tunghuynh314@gmail.com>
2020-08-14 17:35:06 -0500

    Add downloading data function

---
+++ scripts/trends/download.py
@@ -0,0 +1,7 @@
+import pandas as pd                        
+from pytrends.request import TrendReq
+import config
+
+def download_trends(country="US", state=None):
+    pytrend = TrendReq()
+    pass
